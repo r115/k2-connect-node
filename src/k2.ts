d50c0e19b6dc8b888ed0ca087a66325a7bd33d81
Bernard Nandwa <r115codes@gmail.com>
2023-06-04 15:35:36 +0300

    refactor: rebuild functionality around a class.

---
+++ src/k2.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export type K2Options = {
+	clientId: string;
+	clientSecret: string;
+	apiKey: string;
+	baseUrl: string;
+}
+
+/**
+ * Core functionality of the Kopokopo k2 api wrapped in a composable class.
+ */
+class K2Core {
+	version: string = "v1";
+
+	options: K2Options;
+
+	constructor(options: K2Options) {
+		this.options = options;
+	}
+
+	validateInitializationData(options: unknown) {
+		const ApiOptionsData = z.object({
+			clientId: z.string(),
+			clientSecret: z.string(),
+			apiKey: z.string(),
+			baseUrl: z.string()
+		});
+	
+		const isValidData = ApiOptionsData.parse(options);
+	
+		return isValidData;
+	};
+}
+
+export function Kopokopo(options: K2Options) {
+	return new K2Core(options);
+}
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,19 @@
-describe('module initialization', () => {
+import { K2, K2Options } from "../k2";
+
+describe('K2 module initialization', () => {
+	const testCredentials: K2Options = {
+		clientId: "",
+		clientSecret: "",
+		apiKey: "",
+		baseUrl: ""
+	}
+
 	it('runs a test', () => {
-		expect(true).toBeTruthy
+		expect(() => K2({
+			clientId: "",
+			clientSecret: "",
+			apiKey: "",
+			baseUrl: ""
+		})).toThrowError
 	});
 })
